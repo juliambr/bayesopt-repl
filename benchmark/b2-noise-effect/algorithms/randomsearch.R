d01df8adc1850bdd0a3f7914bf2ed1ffa88cbf7b
juliambr <ju.moosbauer@googlemail.com>
2019-12-19 10:41:54 +0100

    fix bug rs

--- benchmark/b2-noise-effect/algorithms/randomsearch.R
+++ benchmark/b2-noise-effect/algorithms/randomsearch.R
@@ -13,9 +13,15 @@ fun = function(data, job, instance) {
 	opdf = opdf[opdf$dob == 0, ]
 	opdf$dob = 1:nrow(opdf)
     
-    if ("smoof_noisy_single_objective_function" %in% class(data)) {
-      opdf$ytrue = apply(matrix(opdf[, getParamIds(par.set)], ncol = getParamLengths(par.set)), 1, getMeanFunction(data))
+  if ("smoof_noisy_single_objective_function" %in% class(data)) {
+    pl = getParamLengths(par.set)
+    pn = getParamIds(par.set)
+    if(pl > 1) {
+      opdf$ytrue = apply(as.matrix(opdf[, paste(pn, 1:pl, sep = "")], ncol = pl), 1, getMeanFunction(data))
+    } else {
+      opdf$ytrue = apply(matrix(opdf[, getParamIds(par.set)], ncol = getParamLengths(par.set)), 1, getMeanFunction(data)) 
     }
+  }
 
   	res = computeFictionalReturnValue(par.set, run, data)
   	opdf = merge(opdf, res, all.x = TRUE, by = "dob")
