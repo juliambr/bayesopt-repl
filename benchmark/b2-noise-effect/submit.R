baa29dc46f977d1181b34a4073b754ac197620cd
juliambr <ju.moosbauer@googlemail.com>
2019-12-19 09:44:42 +0100

    prepare benchmark

--- benchmark/b2-noise-effect/submit.R
+++ benchmark/b2-noise-effect/submit.R
@@ -6,13 +6,19 @@ library(batchtools)
 
 # Load Registry and Metadata
 reg = loadRegistry("registry", writeable = TRUE)
-reg$cluster.functions = makeClusterFunctionsSocket(2)
+# reg$cluster.functions = makeClusterFunctionsSocket(2)
 
-variants = unlist(lapply(ades, function(x) names(x)))
 tab = summarizeExperiments(
-	by = c("job.id", "algorithm", "problem", variants))
+	by = c("job.id", "algorithm", "problem", "repls"))
 
 tosubmit = ijoin(tab, findNotDone())
 tosubmit = tosubmit[repls == 1, ]
 
-submitJobs(tosubmit)
+resources.serial = list(
+	walltime = 3600L * 96L, memory = 1024L * 4L,
+	clusters = "serial", max.concurrent.jobs = 1000L # get name from lrz homepage)
+)
+
+tosubmit$chunk = chunk(tosubmit$job.id, chunk.size = 20)
+
+submitJobs(tosubmit, resources = resources.serial)
